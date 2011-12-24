037b4549bd962b1142de6970feedec075d4de796
Johann Baudy <johaahn@gnu-log.net>
2011-12-24 12:25:50 +0100

    - updated license stuff
    - removed qt files

--- src/qt/window.h
+++ src/qt/window.h
@@ -1,3 +1,24 @@
+/*
+ * window.h
+ *
+ *  Created on: 4 sept. 2011
+ *
+ * Copyright (C) 2010 <Johann Baudy> johann.baudy@gnu-log.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
 #ifndef MYWINDOW_H
 #define MYWINDOW_H
 
